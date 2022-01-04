681cb7ec04646f3d51726d6577de4d3efe1d93b2
HIBER97 <hibar9710@gmail.com>
2022-01-04 10:39:14 +0900

    Update 22.01.04 10:35 ------------------------------- Fix : Can't move to 'takePic' page App shutdown (Did't add 'takepic' in Manifest) Camara Function move to 'takePic.java' ------------------------------- Error :

--- app/src/main/java/com/example/personalproject/MainActivity.java
+++ app/src/main/java/com/example/personalproject/MainActivity.java
@@ -1,26 +1,15 @@
 package com.example.personalproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.nfc.Tag;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    final String TAG = "GILBOMI";
-    String mCurrentPhotoPath;
-    final int REQUEST_TAKE_PHOTO = 01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,36 +22,18 @@ public class MainActivity extends AppCompatActivity {
         btnPicMove = (Button)findViewById(R.id.btnPicMove);
         btnSet = (Button)findViewById(R.id.btnSet);
 
-        //권한 확인
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-            {
-                Log.d(TAG, "권한 설정 완료");
-            } else {
-                Log.d(TAG, "권한 설정 요청");
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1); } }
 
 
         btnPicMove.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
                 //takePic으로 이동
-                Intent takePic = new Intent (getApplicationContext(), takePic.class);
-                startActivity(takePic);
+                Intent intent = new Intent (getApplicationContext(), takePic.class);
+                startActivity(intent);
             }
     });
     }
-
-
-        // 권한 요청
-        @Override
-        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            Log.d(TAG, "onRequestPermissionsResult");
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED ) {
-                Log.d(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
-            } }
-
 
 }
 
--- app/src/main/java/com/example/personalproject/takePic.java
+++ app/src/main/java/com/example/personalproject/takePic.java
@@ -1,28 +1,49 @@
 package com.example.personalproject;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 
 public class takePic extends AppCompatActivity {
 
+    final String TAG = "HIBAR";
     final int TAKE_PICTURE = 1;
+    String mCurrentPhotoPath;
+    final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-
-        Button btnPic;
-        btnPic = (Button)findViewById(R.id.btnPic);
+    protected void onCreate(Bundle savedInstanceState) {
 
         //화면 이동
         super.onCreate(savedInstanceState);
         setContentView(R.layout.takepic);
+
+        Button btnPic;
+        btnPic = (Button) findViewById(R.id.btnPic);
+
+        //권한 확인
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            {
+                Log.d(TAG, "권한 설정 완료");
+            } else {
+                Log.d(TAG, "권한 설정 요청");
+                ActivityCompat.requestPermissions(takePic.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1); }
+        }
+
 
         //버튼 리스터
         btnPic.setOnClickListener(new View.OnClickListener() {
@@ -30,13 +51,23 @@ public class takePic extends AppCompatActivity {
             public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.btnPic:
-                        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                         startActivityForResult(cameraIntent, TAKE_PICTURE);
                         break;
                 }
             }
         });
     }
+
+    // 권한 요청
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult");
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED ) {
+            Log.d(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
+        } }
+
 
     // 카메라로 촬영한 사진의 썸네일을 가져와 이미지뷰에 띄워줌
     @Override protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
